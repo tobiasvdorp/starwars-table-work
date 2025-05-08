279953e85970c65358b0eacff2dbc571c46aa292
HunterRvB <114484205+HunterRvB@users.noreply.github.com>
2025-05-08 15:09:46 +0200

    !

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="grid place-items-center min-h-screen p-4 sm:p-8">
-      <div className="bg-primary rounded-lg shadow-lg p-4 sm:p-8 w-full max-w-2xl">
+      <div className="bg-primary rounded-lg shadow-lg p-4 sm:p-8 w-full max-w-2xl text-center">
         <h1 className="text-2xl sm:text-4xl font-bold text-center mb-4 sm:mb-8 text-gray-800">
           Page Not Found
         </h1>
@@ -11,7 +11,10 @@ export default function NotFound() {
           {" "}
           The page you are looking for does not exist.
         </p>
-        <Link href="/" className="text-black-500 hover:underline">
+        <Link
+          href="/"
+          className="text-black-500 hover:underline border-2 border-black rounded-md px-4 py-2"
+        >
           Go back to the homepage
         </Link>
       </div>
