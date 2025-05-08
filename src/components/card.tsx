b5ba699e524beb586095039bcf1c27707cc4e54e
HunterRvB <114484205+HunterRvB@users.noreply.github.com>
2025-05-08 14:32:11 +0200

    First commit

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,19 +1,19 @@
 export type Character = {
-    uid: string;
-    name: string;
-    url: string;
-}
+  uid: string;
+  name: string;
+  url: string;
+};
 
 type CardProps = {
-    character: Character;
+  character: Character;
 };
 
 export default function Card({ character }: CardProps) {
-    const { uid, name, url } = character;
-    return (
-        <li className="card text-center">
-            <h5>A</h5>
-            <p>{name}</p>
-        </li>
-    )
+  const { uid, name, url } = character;
+  return (
+    <li className="card text-center">
+      <h5>{name.charAt(0)}</h5>
+      <p>{name}</p>
+    </li>
+  );
 }
